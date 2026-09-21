efac91df8eb4b60fbf35d7ecb07fe43e5be02b26
workFLOw42 <workFLOw42@users.noreply.github.com>
2026-09-21 15:57:30 +0200

    Release 1.0.1: targetSdk 36, versionCode 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,9 @@ android {
     defaultConfig {
         applicationId = "de.workflow42.meinenoten"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
